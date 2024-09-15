364d5c21f26a213b087f954840b22a769e9606fe
Aksgo <146610691+Aksgo@users.noreply.github.com>
2024-09-15 21:04:02 +0530

    Create AtCoderMagics.cpp

---
+++ AtCoderMagics.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define veci vector<int>
+#define pii pair<int,int>
+#define rep(i,st,n) for(int i=st; i<n; i++)
+#define ceil(x,y) (x+y-1)/y//least integer function
+#define sz(x) (int)(x).size()
+#define PI 3.141592653589793238 
+#define ull unsigned long long
+#define pb push_back
+#define pp pop_back
+#define IM INT_MAX
+#define IN INT_MIN
+#define lb lower_bound
+#define ub upper_bound
+const long long MOD  = 1e9 + 7;
+const long long MOD1 = 998244353;
+struct ch {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+bool cmp(pair<pii,int> p1, pair<pii, int> p2){
+	return p1.first.first <= p2.first.first;
+}
+void solve(){
+	//code here
+	int n;
+	cin>>n;
+	vector<pair<pii, int>> a(n);
+	rep(i,0,n){
+		int x,y;
+		cin>>x>>y;
+		a[i] = {{x,y}, i};
+	}	
+	int st = n-1;
+	sort(a.begin(), a.end(), cmp);
+	for(int i=n-2; i>=0; --i){
+		auto p1 = a[st].first;
+		auto p2 = a[i].first;
+		if(p1.second>p2.second){st=i; continue;}
+		a[i].second = -1;
+	}
+	set<int> ste;
+	rep(i,0,n){
+		if(a[i].second!=-1){ste.insert(a[i].second+1);}
+	}
+	cout<<ste.size()<<endl;
+	for(auto &ele : ste){
+		cout<<ele<<" ";
+	}
+}
+
+
+int main(){
+	//setIO("diamond");
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);//making cin faster
+	cout.tie(0);
+	clock_t start = clock();
+	int t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+	double duration = ((double) clock() - start) / (double) CLOCKS_PER_SEC;
+	//cout << "\nTime: " << fixed << setprecision(20) << duration*1000<< " ms" << endl;
+}
