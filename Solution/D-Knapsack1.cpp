88398d4527606b0cfbbfefe340c7fbfaa3ce67dd
Aksgo <146610691+Aksgo@users.noreply.github.com>
2024-10-11 13:02:59 +0530

    Create D-Knapsack1.cpp

---
+++ Solution/D-Knapsack1.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define veci vector<int>
+#define pii pair<int,int>
+#define rep(i,st,n) for(int i=st; i<n; i++)
+#define ceil(x,y) (x+y-1)/y//least integer function
+#define sz(x) (int)(x).size()
+#define PI 3.141592653589793238 
+#define ull unsigned long long
+#define pb push_back
+#define pp pop_back
+#define IM INT_MAX
+#define IN INT_MIN
+#define lb lower_bound
+#define ub upper_bound
+const long long MOD  = 1e9 + 7;
+const long long MOD1 = 998244353;
+int nxt(){
+	int x;cin>>x;return x;
+}
+ll nxl(){
+	ll x;cin>>x;return x;
+}
+struct ch {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+int wt[105];
+int val[105];
+ll dp[101][100005];
+ll func(int ind, int weight){
+	if(weight==0 || ind<0)return 0;
+	if(dp[ind][weight]!=-1)return dp[ind][weight];
+	ll ans = func(ind-1, weight);
+	if(weight-wt[ind]>=0){
+		ans = max(ans, func(ind-1,weight-wt[ind])+val[ind]);
+	}
+	dp[ind][weight]=ans;
+	return ans;
+}
+void solve(){
+	//code here
+	memset(dp, -1, sizeof(dp));
+	int n,w;
+	cin>>n>>w;
+	rep(i,0,n){
+		cin>>wt[i]>>val[i];
+	}
+	cout<<func(n-1, w);
+}
+
+
+
+int main(){
+	//setIO("");
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);//making cin faster
+	clock_t start = clock();
+	int t = 1;
+	//cin>>t;
+	while(t--)	
+		solve();
+	double duration = ((double) clock() - start) / (double) CLOCKS_PER_SEC;
+	//cout << "\nTime: " << fixed << setprecision(20) << duration*1000<< " ms" << endl;
+}
